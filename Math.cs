b038281fadb5eab149fd49e61507f709df218284
jmcota <jmcota@ontica.org>
2017-03-01 23:35:48 -0500

    Initial version

---
+++ Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AppVeyorProject {
+
+    public class Math {
+
+    static public int Sum(int x, int y) {
+      return x + y;
+    }
+
+    static public int Substract(int x, int y) {
+      return x - y;
+    }
+
+    static public int Multiply(int x, int y) {
+      return x * y;
+    }
+
+    static public int Divide(int x, int y) {
+      return x / y;
+    }
+
+
+  }
+
+}
